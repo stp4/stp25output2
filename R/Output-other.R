8920974dd8092ca9a3849c069e24b6a03e0a24bc
stp4 <w.peter@statistik-peter.at>
2023-10-31 06:14:36 +0100

    neu sjTable

--- R/Output-other.R
+++ R/Output-other.R
@@ -21,6 +21,44 @@ Output.gtsummary <- function(x,
   Output(rst,
          caption = caption,
          note = note,
-         col.names = names_rst,
+         header = names_rst,
          ...)
+}
+
+
+
+
+#' @rdname Output
+#'
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'
+#' library(sjPlot)
+#' library(sjmisc)
+#' library(sjlabelled)
+#'
+#' data("efc")
+#' efc <- as_factor(efc, c161sex, c172code)
+#'
+#' m1 <- lm(barthtot ~ c160age + c12hour + c161sex + c172code, data = efc)
+#' m2 <- lm(neg_c_7 ~ c160age + c12hour + c161sex + e17age, data = efc)
+#'
+#' sjPlot::tab_model(m1, m2) %>%
+#'   Output("Side-by-side Regression Models")
+#' }
+#'
+Output.sjTable <- function(x,
+                           caption = NULL,
+                           note = NULL,
+                         #  col.names = NULL,
+                           output = which_output(),
+                           ...) {
+  caption <- Caption(caption)
+  Text(caption)
+  if (output == "text")
+    x
+  else
+    x$knitr  %>% HTML_default()
 }
